34e0905bd655f9461420b1aa1f747685bd145aad
luiza-omena <99267967+luiza-omena@users.noreply.github.com>
2022-10-25 11:26:09 -0300

    Adicionado a biblioteca django-heroku

--- swep/settings.py
+++ swep/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+#importando djangoheroku
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -126,4 +128,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+django_heroku.settings(locals())
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
