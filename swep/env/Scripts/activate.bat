5ba62f10e3d0ad5175070d4ab008967a164c22c2
luiza-omena <99267967+luiza-omena@users.noreply.github.com>
2022-11-19 10:49:28 -0300

    Ajustes

--- swep/env/Scripts/activate.bat
+++ swep/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\suass\swep\env
+set VIRTUAL_ENV=C:\Users\suass\OneDrive\Documents\GitHub\SWEP\swep\env
 
 if not defined PROMPT set PROMPT=$P$G
 
